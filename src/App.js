6c477e9711ca1236bc05b3a9e1ba08a36e1de4ca
Peter Mercer <Peter.Mercer@nps.org.au>
2018-10-09 14:10:49 +1100

    First Basic implementation of Draft added draft into App.js and crated a BOLD and ITALIC Button Also commented out an import of editor content

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Editor, EditorState, RichUtils, convertToRaw, convertFromRaw} from 'draft-js';
 
 class App extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {editorState: EditorState.createEmpty()};
+        this.onChange = (editorState) => this.setState({editorState});
+
+        /*
+
+        var test = '{"blocks":[{"key":"3h6ei","text":"This is a BOLD test","type":"unstyled","depth":0,"inlineStyleRanges":[{"offset":10,"length":4,"style":"BOLD"}],"entityRanges":[],"data":{}}],"entityMap":{}}';
+
+        var blocks = convertFromRaw(JSON.parse(test));
+        this.state = {editorState: EditorState.createWithContent(blocks)};
+         */
+    }
+
+    _onBoldClick() {
+        this.onChange(RichUtils.toggleInlineStyle(
+            this.state.editorState,
+            'BOLD'
+    ));
+
+        const content = this.state.editorState.getCurrentContent();
+        console.log(JSON.stringify(convertToRaw(content)));
+    }
+
+    _onItalicClick() {
+        this.onChange(RichUtils.toggleInlineStyle(
+            this.state.editorState,
+            'ITALIC'
+        ));
+
+        const content = this.state.editorState.getCurrentContent();
+        console.log(JSON.stringify(convertToRaw(content)));
+    }
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+
+
+          <div className="editor">
+              <button onClick={this._onBoldClick.bind(this)}>Bold</button>
+              <button onClick={this._onItalicClick.bind(this)}>Italic</button>
+          <Editor
+              editorState={this.state.editorState}
+              onChange={this.onChange}
+          />
+
+      </div>
       </div>
     );
   }
