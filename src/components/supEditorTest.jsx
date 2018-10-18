a72ce2c16cab880b36cba4a34e018987fcb1f97c
Peter Mercer <Peter.Mercer@nps.org.au>
2018-10-18 17:18:38 +1100

    Added Ref and Sub editors

--- src/components/supEditorTest.jsx
+++ src/components/supEditorTest.jsx
@@ -1,6 +1,19 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import {Editor, EditorState, RichUtils, convertToRaw, CompositeDecorator} from 'draft-js';
 
+
+function findSupEntries(contentBlock, callback, contentState) {
+    contentBlock.findEntityRanges(
+        (character) => {
+            const entityKey = character.getEntity();
+            return (
+                entityKey !== null &&
+                contentState.getEntity(entityKey).getType() === 'SUP'
+            );
+        },
+        callback
+    );
+}
 
 function findSubEntries(contentBlock, callback, contentState) {
     contentBlock.findEntityRanges(
@@ -8,7 +21,20 @@ function findSubEntries(contentBlock, callback, contentState) {
             const entityKey = character.getEntity();
             return (
                 entityKey !== null &&
-                contentState.getEntity(entityKey).getType() === 'SUP'
+                contentState.getEntity(entityKey).getType() === 'SUB'
+            );
+        },
+        callback
+    );
+}
+
+function findRefEntries(contentBlock, callback, contentState) {
+    contentBlock.findEntityRanges(
+        (character) => {
+            const entityKey = character.getEntity();
+            return (
+                entityKey !== null &&
+                contentState.getEntity(entityKey).getType() === 'REFNUM'
             );
         },
         callback
@@ -24,7 +50,32 @@ const Sup = (props) => {
     );
 };
 
+const Sub = (props) => {
+
+    return (
+        <sub>
+            {props.children}
+        </sub>
+    );
+};
+
+const RefNum = (props) => {
+    return (
+        <span style={styles.reference} >
+            {props.children}
+        </span>
+    );
+}
+
 const styles = {
+    reference: {
+        fontSize: '10px',
+        padding:0,
+        margin:0,
+        position:'relative',
+        verticalAlign:'top',
+        top: '-11px'
+    },
     root: {
         fontFamily: '\'Georgia\', serif',
         padding: 20,
@@ -64,10 +115,18 @@ class SupEditorExample extends Component {
 
         const decorator = new CompositeDecorator([
             {
-                strategy: findSubEntries,
+                strategy: findSupEntries,
                 component: Sup
+            },
+            {
+                strategy: findSubEntries,
+                component: Sub
+            },
+            {
+                strategy: findRefEntries,
+                component: RefNum
             }
-        ])
+        ]);
 
         this.state = {
             editorState: EditorState.createEmpty(decorator)
@@ -81,64 +140,132 @@ class SupEditorExample extends Component {
         };
 
         this.addSup = this._addSup.bind(this);
+        this.addSub = this._addSub.bind(this);
+        this.addRef = this._addRef.bind(this);
+        this.removeSup = this._removeSup.bind(this);
+
     }
 
-        _addSup(e) {
-            e.preventDefault();
-            const {editorState} = this.state;
-            const contentState = editorState.getCurrentContent();
-            const contentStateWithEntity = contentState.createEntity(
-                'SUP',
-                'MUTABLE'
-            );
-            const entityKey = contentStateWithEntity.getLastCreatedEntityKey();
-            const newEditorState = EditorState.set(editorState, { currentContent: contentStateWithEntity });
+    _addSup(e) {
+        e.preventDefault();
+
+        const {editorState} = this.state;
+        const contentState = editorState.getCurrentContent();
+        const contentStateWithEntity = contentState.createEntity(
+            'SUP',
+            'MUTABLE'
+        );
+        const entityKey = contentStateWithEntity.getLastCreatedEntityKey();
+        const newEditorState = EditorState.set(editorState, {currentContent: contentStateWithEntity});
+        this.setState({
+            editorState: RichUtils.toggleLink(
+                newEditorState,
+                newEditorState.getSelection(),
+                entityKey
+            )
+        }, () => {
+            setTimeout(() => this.refs.editor.focus(), 0);
+        });
+    }
+
+    _addSub(e) {
+        e.preventDefault();
+
+        const {editorState} = this.state;
+        const contentState = editorState.getCurrentContent();
+        const contentStateWithEntity = contentState.createEntity(
+            'SUB',
+            'MUTABLE'
+        );
+        const entityKey = contentStateWithEntity.getLastCreatedEntityKey();
+        const newEditorState = EditorState.set(editorState, {currentContent: contentStateWithEntity});
+        this.setState({
+            editorState: RichUtils.toggleLink(
+                newEditorState,
+                newEditorState.getSelection(),
+                entityKey
+            )
+        }, () => {
+            setTimeout(() => this.refs.editor.focus(), 0);
+        });
+    }
+
+    _removeSup(e) {
+        e.preventDefault();
+        const {editorState} = this.state;
+        const selection = editorState.getSelection();
+        if (!selection.isCollapsed()) {
             this.setState({
-                editorState: RichUtils.toggleLink(
-                    newEditorState,
-                    newEditorState.getSelection(),
-                    entityKey
-                )
-            }, () => {
-                setTimeout(() => this.refs.editor.focus(), 0);
+                editorState: RichUtils.toggleLink(editorState, selection, null),
             });
 
         }
 
-        render() {
-            return (
-                <div>
-                    <button style={styles.button} onMouseDown={this.addSup}>
-                        Sup
-                    </button>
-                    <div style={styles.editor} onClick={this.focus}>
-                        <Editor
-                            editorState={this.state.editorState}
-                            onChange={this.onChange}
-                            placeholder="Enter some text..."
-                            ref="editor"
-                        />
-                    </div>
-                    <input
-                        onClick={this.logState}
-                        style={styles.button}
-                        type="button"
-                        value="Log State"
+    }
+
+    _addRef(e) {
+        e.preventDefault();
+
+        const {editorState} = this.state;
+        const contentState = editorState.getCurrentContent();
+        const contentStateWithEntity = contentState.createEntity(
+            'REFNUM',
+            'MUTABLE'
+        );
+        const entityKey = contentStateWithEntity.getLastCreatedEntityKey();
+
+        const newEditorState = EditorState.set(editorState, {currentContent: contentStateWithEntity});
+        console.log(JSON.stringify(newEditorState.getSelection()));
+        this.setState({
+            editorState: RichUtils.toggleLink(
+                newEditorState,
+                newEditorState.getSelection(),
+                entityKey
+            )
+        }, () => {
+            setTimeout(() => this.refs.editor.focus(), 0);
+        });
+    }
+
+
+
+    render() {
+        return (
+            <div>
+                <button style={styles.button} onMouseDown={this.addSup}>
+                    Sup
+                </button>
+                <button style={styles.button} onMouseDown={this.addSub}>
+                    Sub
+                </button>
+                <button style={styles.button} onMouseDown={this.addRef}>
+                    Ref
+                </button>
+                <button style={styles.button} onMouseDown={this.removeSup}>
+                    Remove
+                </button>
+
+
+                <div style={styles.editor} onClick={this.focus}>
+                    <Editor
+                        editorState={this.state.editorState}
+                        onChange={this.onChange}
+                        placeholder="Enter some text..."
+                        ref="editor"
                     />
                 </div>
-            );
-        }
-
-
-
-
-
+                <input
+                    onClick={this.logState}
+                    style={styles.button}
+                    type="button"
+                    value="Log State"
+                />
+            </div>
+        );
+    }
 
 
 }
-
-
-
 
 
 export default SupEditorExample;
